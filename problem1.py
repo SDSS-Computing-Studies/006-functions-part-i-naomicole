11f931ddce9b11301e04bb6465a553cad4400a73
naomicole <71406713+naomicole@users.noreply.github.com>
2020-10-07 13:02:36 -0700

    .

--- problem1.py
+++ problem1.py
@@ -7,3 +7,25 @@ If the boolean is False, then the larger number is the hypotenuse
 Return the missing side
 (2 points)
 """
+
+import math
+
+def hypotenuse(a,b,c):
+    if False:
+        d=(a**2)+(b**2)
+        answer= int(math.sqrt(d))
+        return answer
+
+    elif True:
+        a,b.sort
+        n=a,b[0]
+        m=a,b[1]
+        o=(m**2)-(n**2)
+        answer=int(math.sqrt(o))
+        return answer
+
+x=hypotenuse(3,4,False)
+print(x)
+
+
+
