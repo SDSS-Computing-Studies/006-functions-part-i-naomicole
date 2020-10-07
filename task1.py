4e03b45e1420259afd5dffaf6cf15d6c333963a2
naomicole <71406713+naomicole@users.noreply.github.com>
2020-10-07 12:20:43 -0700

    .

--- task1.py
+++ task1.py
@@ -1,15 +1,15 @@
-#!python3
+#! python3
 
 def sum(a,b):
-    #inputs
-    # a : float 
-    # b : float
-    # return value: returns the sum of the 2 numbers
+    answer=a+b
+    return answer
 
 
 
 #this should return a value of 7
 x = sum(3,4)
+print(x)
 
 #this should return a value of 12.5
 y = sum(11,1.5)
+print(y)
--- task2.py
+++ task2.py
@@ -6,3 +6,11 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
+
+def largest():
+    largest.sort()
+    answer=largest[-1]
+    return answer
+
+x=largest([1,2,3,4,5])
+print(x)
