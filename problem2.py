6f72dd66b1fc90bf5f8ed3cb1590e9b9bfba0f0c
naomicole <71406713+naomicole@users.noreply.github.com>
2020-10-08 12:04:15 -0700

    .

--- problem2.py
+++ problem2.py
@@ -9,3 +9,23 @@ Note that the coordinates should be signed (positive or negative) floats
 """
 import math
 
+def distance(a,b,c):
+    x1=a[0]
+    y1=a[1]
+    x2=b[0]
+    y2=b[1]
+    
+    x=x2-x1
+    y=y2-y1
+
+    z=(x**2)+(y**2)
+    num=math.sqrt(z)
+    answer=round(num,c)
+    return answer
+
+x=distance((-3,2.2) , (1,2) , 3)
+print(x)
+
+
+
+
